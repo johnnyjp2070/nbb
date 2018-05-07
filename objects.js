4f77c169964266999fad57c18a15427f84283ed4
Johnson <johnnyjp2070@gmail.com>
2018-05-07 17:30:25 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+var pizza = {
+    toppings: ['cheese', 'sauce', 'pepperoni'],
+    crust: 'deep dish',
+    serves: 2
+};
+console.log(pizza);
+=======
+var address = {
+    city:"Bangalore",
+    state:"Bangalore"
+};
+var player = {
+    "name" : "Johnson",
+    "age"  : 28,
+    "runs"  : 3500,
+     address: address
+};
+
+console.log(player);
+>>>>>>> 2f89abef1318aaeaec251ac0e7708bf726b9ed3f
